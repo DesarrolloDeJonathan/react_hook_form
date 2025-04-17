96743a4c813ac5765320197714ad0bec2fa2ff41
JoTanDev <jonathan.cardozo.dev@gmail.com>
2025-04-17 16:30:06 -0500

    Structuring files

--- src/CustomForm.tsx
+++ src/CustomForm.tsx
@@ -1,6 +1,6 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { SubmitHandler, useForm } from 'react-hook-form';
-import InputForm from './components/CustomInput';
+import InputForm from './components/components/CustomInput';
 import { z } from 'zod';
 
 const schema = z.object({
@@ -30,6 +30,7 @@ const CustomForm = () => {
       <InputForm name="email" control={control} label="Email" type="email" error={errors.email}/>
       <InputForm name="password" control={control} label="Password" type="password" error={errors.password}/>
       <InputForm name="confirmPassword" control={control} label="Confirm Password" type="password" error={errors.confirmPassword}/>
+      <button type="submit">Submit</button>
     </form>      
     )
 }
---
+++ src/components/CustomInput.tsx
@@ -0,0 +1,34 @@
+import { Control, Controller, FieldError } from "react-hook-form";
+import './CustomInput.css'
+
+interface Props {
+  name: string;
+  control: Control<any>;
+  label: string;
+  type?: string;
+  error?: FieldError;  
+}
+
+const InputForm = ({ name, control, label, type, error }: Props) => {
+  return (
+    <div className="form-group">
+      <label htmlFor={name}>{label}</label>
+      <Controller
+        name={name} 
+        control={control}
+        render={({ field }) => (
+          <input 
+            {...field}
+            id={name}
+            type={type}
+            // placeholder="Enter your name" 
+            className={`border form-control ${error ? '"is not valid"border-red-500' : '" "border-gray-300'} rounded-md p-2 w-full` }
+          />
+        )}
+      />
+      {error && <p className="text-red-500">{error.message}</p>}
+    </div>
+  );
+}
+
+export default InputForm;
